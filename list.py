516ad46ff388e9f57cdba1d5748551ce6d0ed591
22wittthawin <witthawinhomon@gmail.com>
2025-08-25 14:11:26 +0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+def sum_of_squares(numbers):
+    total = 0
+    for num in numbers:
+        total += num ** 2
+    return total
+
+my_list = [50, 500, 5000]
+
+result = sum_of_squares(my_list)
+print("ผลบวกกำลังสอง =", result)
